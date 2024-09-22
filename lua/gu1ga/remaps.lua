d53946b718d8ef3b1ed1284df8ac593de8b3bc79
Guilherme Gomes <guigagmg@gmail.com>
2024-09-21 22:12:11 -0300

    add new remaps

--- lua/gu1ga/remaps.lua
+++ lua/gu1ga/remaps.lua
@@ -1,5 +1,5 @@
-print("asdasda")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-print("im loaded")
-
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
