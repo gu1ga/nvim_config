5c9da7142a9026345db1aef404c01a48cfe01e9f
gu1ga <guigagmg@gmail.com>
2024-10-01 23:59:29 -0300

    setup lualine and modicator

--- lua/gu1ga/plugins/colorscheme.lua
+++ lua/gu1ga/plugins/colorscheme.lua
@@ -1,12 +1,17 @@
-return {
-	'loctvl842/monokai-pro.nvim',
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("monokai-pro").setup({
-			transparent_background = true,
-			filter = "classic"
-		})
+color_scheme = 'loctvl842/monokai-pro.nvim'
+return
+{
+	{
+		color_scheme,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('monokai-pro').setup({
+				transparent_background = true,
+				filter = 'classic',
+			})
 		vim.cmd([[colorscheme monokai-pro]])
-	end
-	}
+		end
+	},
+	color_scheme
+}
