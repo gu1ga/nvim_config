57491cc1acd8b9f51091360ce095e28c3c678c27
Guilherme Gomes <guigagmg@gmail.com>
2024-10-07 23:28:34 -0300

    update lsp config

--- lua/gu1ga/plugins/lsp.lua
+++ lua/gu1ga/plugins/lsp.lua
@@ -17,10 +17,10 @@ return {
 		local cmp = require('cmp')
 		local cmp_lsp = require("cmp_nvim_lsp")
 		local capabilities = vim.tbl_deep_extend(
-		"force",
-		{},
-		vim.lsp.protocol.make_client_capabilities(),
-		cmp_lsp.default_capabilities())
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities())
 
 		require("fidget").setup({})
 		require("mason").setup()
@@ -33,27 +33,12 @@ return {
 			},
 			handlers = {
 				function(server_name) -- default handler (optional)
+--
 					require("lspconfig")[server_name].setup {
-						capabilities = capabilities
+						capabilities = capabilities,
 					}
 				end,
 
-				zls = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.zls.setup({
-						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-						settings = {
-							zls = {
-								enable_inlay_hints = true,
-								enable_snippets = true,
-								warn_style = true,
-							},
-						},
-					})
-					vim.g.zig_fmt_parse_errors = 0
-					vim.g.zig_fmt_autosave = 0
-
-				end,
 				["lua_ls"] = function()
 					local lspconfig = require("lspconfig")
 					lspconfig.lua_ls.setup {
--- lua/gu1ga/set.lua
+++ lua/gu1ga/set.lua
@@ -1,4 +1,5 @@
 vim.opt.nu = true
+		print("Comecou")
 vim.opt.relativenumber = true
 
 vim.opt.hlsearch = false
@@ -14,7 +15,23 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.filetype.add {
-	extension ={
+	extension = {
 		PY = 'python',
 	},
 }
+
+
+--LSP STUFF
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = args.buf })
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = args.buf })
+		vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { buffer = args.buf })
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = args.buf })
+		vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { buffer = args.buf })
+		vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { buffer = args.buf })
+		vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, { buffer = args.buf })
+	end,
+})
