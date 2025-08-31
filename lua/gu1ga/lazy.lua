8b19f9dc3fa4b63078c3c42069c80db652195dea
Guilherme Gomes <guigagmg@gmail.com>
2025-08-31 11:43:27 -0300

    remove double leader remap declaration

--- lua/gu1ga/lazy.lua
+++ lua/gu1ga/lazy.lua
@@ -14,8 +14,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 require("lazy").setup("gu1ga.plugins")
-
