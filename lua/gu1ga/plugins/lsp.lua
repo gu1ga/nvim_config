ccc9f44a409040e31ca42d9ff4850ed7be982330
Guilherme Gomes <guigagmg@gmail.com>
2025-08-31 11:53:04 -0300

    disable luasnip

--- lua/gu1ga/plugins/lsp.lua
+++ lua/gu1ga/plugins/lsp.lua
@@ -59,11 +59,11 @@ return {
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
+			-- snippet = {
+			-- 	expand = function(args)
+			-- 		require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- 	end,
+			-- },
 			mapping = cmp.mapping.preset.insert({
 				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -72,7 +72,7 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' }, -- For luasnip users.
+				-- { name = 'luasnip' }, -- For luasnip users.
 			}, {
 				{ name = 'buffer' },
 			})
